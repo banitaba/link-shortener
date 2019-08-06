6f8a2ce8d1b9d5de9e12e9ad201e073481f335a7
Hossein Banitaba <h.banitaba@gmail.com>
2019-08-06 06:19:38 -0700

    ver 2.1 Usage of unit test

--- spec/controllers/short_links_controller_spec.rb
+++ spec/controllers/short_links_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ShortLinksController, type: :controller do
-
+    subject(:controllerSample) { ShortLinksController.new }
+    context "output of new method" do
+        it "creates a new model of short_link" do
+            expect(controllerSample.new).to have_attributes( :id => nil, :original_url => nil, :view_count => 0, :created_at => nil, :updated_at => nil)
+        end
+    end
 end
